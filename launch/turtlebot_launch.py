12438eec0e710408e3adc622d77520c9085bfaa2
Oori Schubert <oorischubert@gmail.com>
2024-02-24 01:51:07 +0200

    base_link + a2m8

--- launch/turtlebot_launch.py
+++ launch/turtlebot_launch.py
@@ -13,18 +13,36 @@ def generate_launch_description():
     )
 
     rplidar_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('rplidar_ros'), 'launch', 'rplidar_a1_launch.py')
-        ),
-        launch_arguments={
-            'scan_mode': 'Boost'
-        }.items()
-    )
+    PythonLaunchDescriptionSource(
+        os.path.join(get_package_share_directory('rplidar_ros'), 'launch', 'rplidar_a2m8_launch.py')
+    ),
+    launch_arguments={
+        'scan_mode': 'Boost',
+        'frame_id': 'base_link'  # Set the frame_id for the RPLIDAR
+    }.items()
+)
+
+    static_tf_broadcaster = Node(
+    package='tf2_ros',
+    executable='static_transform_publisher',
+    arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link'],  # Adjust these values as needed
+    name='static_tf_broadcaster'
+)
+
 
     return launch.LaunchDescription([
+        static_tf_broadcaster,
         motor_controller,
         rplidar_launch,
     ])
+
+    #rplidar: 
+    #git clone -b ros2 https://github.com/Slamtec/rplidar_ros.git
+    #ros2 launch rplidar_ros rplidar_a2m8_launch.py frame_id:=base_link
+    
+    #base_link: 
+    #sudo apt install ros-humble-tf2-ros
+    #ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 map base_link
 
     # usb port connections:
     # |------------------|
